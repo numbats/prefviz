3d27e77f41a04b6d8a1d885dfa6af0692d58f72c
linhngo66 <tngo0043@student.monash.edu>
2025-11-27 15:43:43 +1100

    plot how points move through election round

--- inst/explore_ggtern.R
+++ inst/explore_ggtern.R
@@ -9,8 +9,8 @@ url <- "https://results.aec.gov.au/27966/Website/Downloads/HouseDopByDivisionDow
 aec <- read_csv(url, skip = 1) |> 
   filter(CalculationType == "Preference Percent") |> 
   mutate(Party = case_when(
-    !(PartyAb %in% c("LP", "ALP", "NP")) ~ "Other",
-    PartyAb %in% c("LP", "NP") ~ "NLP",
+    !(PartyAb %in% c("LP", "ALP", "NP", "LNP")) ~ "Other",
+    PartyAb %in% c("LP", "NP", "LNP") ~ "LNP",
     TRUE ~ PartyAb
   )) 
 
@@ -33,12 +33,12 @@ aec_pref <- aec |>
       values_from = Votes,
       values_fill = 0
     ) |> 
-    mutate(Other = 100 - NLP - ALP) |>
-    select(DivisionNm, ALP, NLP, Other, ElectedParty)
+    mutate(Other = 100 - LNP - ALP) |>
+    select(DivisionNm, ALP, LNP, Other, ElectedParty)
 
 # Define the region
 alp_region <- tribble(
-  ~ALP,   ~Other, ~NLP,
+  ~ALP,   ~Other, ~LNP,
   33.33,  33.33,  33.33,
   50,     50,    0,
   100,    0,      0,
@@ -46,8 +46,8 @@ alp_region <- tribble(
   33.33,  33.33,  33.33
 )
 
-nlp_region <- tribble(
-  ~ALP,   ~Other, ~NLP,
+lnp_region <- tribble(
+  ~ALP,   ~Other, ~LNP,
   33.33,  33.33,  33.33,
   0,      50,     50,
   0,      0,      100,
@@ -56,7 +56,7 @@ nlp_region <- tribble(
 )
 
 other_region <- tribble(
-  ~ALP,   ~Other, ~NLP,
+  ~ALP,   ~Other, ~LNP,
   33.33,  33.33,  33.33,
   50,     50,    0,
   0,      100,    0,
@@ -64,16 +64,16 @@ other_region <- tribble(
   33.33,  33.33,  33.33
 )
 
-fst_pref_tern <- ggtern(data = aec_pref, aes(x = ALP, y = Other, z = NLP)) +
+fst_pref_tern <- ggtern(data = aec_pref, aes(x = ALP, y = Other, z = LNP)) +
   geom_polygon(data = alp_region,
                 fill = "red", alpha = 0.15) +
-  geom_polygon(data = nlp_region,
+  geom_polygon(data = lnp_region,
               fill = "blue", alpha = 0.15) +
   geom_polygon(data = other_region,
                 fill = "green", alpha = 0.15) +
   geom_point(aes(color = ElectedParty), size = 3, alpha = 0.8) +
   scale_color_manual(
-    values = c("ALP" = "red", "NLP" = "blue", "Other" = "green"),
+    values = c("ALP" = "red", "LNP" = "blue", "Other" = "green"),
     name = "Elected Party"
   ) +
   theme(
@@ -95,9 +95,9 @@ plot_ly(
   aec_pref,
   a = ~Other,
   b = ~ALP, 
-  c = ~NLP,
+  c = ~LNP,
   color = ~ElectedParty,
-  colors = c("ALP" = "red", "NLP" = "blue", "Other" = "green"),
+  colors = c("ALP" = "red", "LNP" = "blue", "Other" = "green"),
   type = "scatterternary",
   mode = "markers",
   marker = list(size = 8),
@@ -105,7 +105,7 @@ plot_ly(
     "<b>", DivisionNm, "</b><br>",
     "ALP: ", round(ALP, 2), "%<br>",
     "Other: ", round(Other, 2), "%<br>",
-    "NLP: ", round(NLP, 2), "%<br>",
+    "LNP: ", round(LNP, 2), "%<br>",
     "Elected: ", ElectedParty
   ),
   hoverinfo = "text"
@@ -115,6 +115,57 @@ plot_ly(
       sum = 100,
       aaxis = list(title = "Other"),
       baxis = list(title = "ALP"),
-      caxis = list(title = "NLP")
+      caxis = list(title = "LNP")
     )
+  )
+
+# Movement across rounds of voting
+aec_round <- aec |> 
+  left_join(
+    aec |> 
+      filter(Elected == "Y") |> 
+      select(DivisionNm, ElectedParty = Party, CountNumber),
+    by = c("DivisionNm", "CountNumber")
+  ) |> 
+  group_by(DivisionNm, CountNumber, Party, ElectedParty) |> 
+  summarise(
+    Votes = sum(CalculationValue, na.rm = TRUE),
+    .groups = "drop"
+  ) |> 
+  pivot_wider(
+    names_from = Party,
+    values_from = Votes,
+    values_fill = 0
+  ) |> 
+  mutate(Other = 100 - LNP - ALP)
+
+ggtern(
+  data = aec_round |> filter(DivisionNm %in% c("Richmond", "Cowper")),
+  aes(x = ALP, y = Other, z = LNP)) +
+  geom_point(
+    aes(color = ElectedParty, shape = DivisionNm),
+    size = 2, alpha = 0.8) +
+  geom_path(
+    aes(group = DivisionNm, color = ElectedParty)
+  ) +
+  geom_polygon(data = alp_region,
+    fill = "red", alpha = 0.15) +
+  geom_polygon(data = lnp_region,
+      fill = "blue", alpha = 0.15) +
+  geom_polygon(data = other_region,
+        fill = "green", alpha = 0.15) +
+  scale_color_manual(
+  values = c("ALP" = "red", "LNP" = "blue", "Other" = "green"),
+  name = "Elected Party"
+  ) +
+  theme(
+  tern.axis.line.T = element_line(color = "green"),
+  tern.axis.line.L = element_line(color = "red"),
+  tern.axis.line.R = element_line(color = "blue"),
+  tern.axis.text.T = element_text(color = "green"),
+  tern.axis.text.L = element_text(color = "red"),
+  tern.axis.text.R = element_text(color = "blue"),
+  tern.axis.title.T = element_text(color = "green"),
+  tern.axis.title.L = element_text(color = "red"),
+  tern.axis.title.R = element_text(color = "blue")
   )
