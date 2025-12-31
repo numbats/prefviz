db37ca9b3404640ef9de16e522ed7fc4795759ed
linhngo66 <tngo0043@student.monash.edu>
2025-12-31 17:57:20 +1100

    minor changes

--- R/add_vertex_labels.R
+++ R/add_vertex_labels.R
@@ -4,7 +4,7 @@
 #' Adds text labels at the vertices of a ternary simplex with automatic
 #' positioning adjustments.
 #'
-#' @param vert A data frame containing vertex coordinates and labels.
+#' @param vertex_labels_df A data frame containing vertex coordinates and labels.
 #'   Should have columns `x1`, `x2`, and `labels`. Can be specified manually or 
 #'   obtained from a ternable object: `ternable_object$alternatives`.
 #' @param ... Arguments passed to [ggplot2::geom_text()], such as
@@ -19,17 +19,17 @@
 #' # Add vertex labels
 #' ggplot() +
 #'   geom_ternary_cart() +
-#'   add_vertex_labels(tern$simplex_vertices)
+#'   add_vertex_labels(tern$alternatives)
 #' 
 #' # Customize label appearance
 #' ggplot() +
 #'   geom_ternary_cart() +
-#'   add_vertex_labels(tern$simplex_vertices, size = 5, fontface = "bold")
+#'   add_vertex_labels(tern$alternatives, size = 5, fontface = "bold")
 #'
 #' @export
-add_vertex_labels <- function(vert, ...) {
+add_vertex_labels <- function(vertex_labels_df, ...) {
   ggplot2::geom_text(
-    data = vert,
+    data = vertex_labels_df,
     mapping = ggplot2::aes(x = .data$x1, y = .data$x2, label = .data$labels),
     nudge_x = c(-0.02, 0.02, 0),
     nudge_y = c(-0.05, -0.05, 0.05),
