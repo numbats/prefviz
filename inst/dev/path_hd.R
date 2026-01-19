3ae9018c8359f7b5e6b205a65477b9459892661e
linhngo66 <tngo0043@student.monash.edu>
2026-01-19 22:44:36 +1100

    demo code for detourr

--- inst/dev/path_hd.R
+++ inst/dev/path_hd.R
@@ -61,3 +61,61 @@ cbind(tern22$data, tern22$data_edges)
 
 add_data_edges(pref_2022, "DivisionNm")
 
+#----- Detour
+
+# Color
+colors <- c(
+  "ALP" = "red",    # Red
+  "LNP" = "blue",    # Blue
+  "GRN" = "green",    # Green
+  "IND" = "orange",    # Orange
+  "Other" = "gray"   # Gray
+)
+
+# First preference scatter
+first_pref_2025 <- read_csv("inst/dev/pref_2025.csv") |> 
+  filter(CountNumber == 0)
+
+tern_first_pref <- ternable(first_pref_2025, ALP:IND)
+
+col_first_pref <- c(rep("black", 5),
+  party_colors[first_pref_2025$Winner])
+
+animate_xy(
+  get_tern_data(tern_first_pref, plot_type = "HD"), 
+  edges = get_tern_edges(tern_first_pref),
+  obs_labels  = get_tern_labels(tern_first_pref),
+  col = col_first_pref,
+  axes = "bottomleft"
+)
+
+d_edges <- get_tern_edges(tern_first_pref)
+dtour_data <- tern_first_pref$simplex_vertices |>
+  mutate(
+    Winner = labels
+  ) |> 
+  bind_rows(get_tern_data(tern_first_pref, plot_type = "2D")) |> 
+  mutate(text = if_else(
+    is.na(labels), 
+    paste0(DivisionNm, "\n",
+          "Elected Party: ", Winner, "\n",
+          "ALP: ", round(ALP*100, 1), "%\n",
+          "LNP: ", round(LNP*100, 1), "%\n",
+          "GRN: ", round(GRN*100, 1), "%\n",
+          "IND: ", round(IND*100, 1), "%\n",
+          "Other: ", round(Other*100, 1), "%"),
+    labels
+  ))
+
+de <- detour(
+  dtour_data,
+  tour_aes(projection = x1:x4, colour = Winner, label = text)
+) |> 
+  tour_path(grand_tour(3), fps = 60) |>
+  show_scatter(
+    axes = FALSE, 
+    palette = party_colors,
+    edges = get_tern_edges(tern_first_pref),
+    size = 1.5
+  )
+de
